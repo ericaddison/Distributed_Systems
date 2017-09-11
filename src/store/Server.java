97c740dfbfc810df823d103f043f32f12a492f0c
ericaddison <ericaddison@gmail.com>
2017-09-11 16:06:55 -0500

    Added object stream to server

--- src/store/Server.java
+++ src/store/Server.java
@@ -3,6 +3,7 @@ package store;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -24,29 +25,33 @@ public class Server {
 	    String fileName = args[2];
 
 	    // parse the inventory file
+	    Inventory inv = new Inventory(fileName);
+	    
+	    
+	    
+	    // SAMPLE TCP CODE
 	    
 	    // set up server tcp socket
+	    // remember need to multi-thread
 	    try (
-            ServerSocket serverSocket =
-                new ServerSocket(Integer.parseInt(args[0]));
+            ServerSocket serverSocket = new ServerSocket(tcpPort);
             Socket clientSocket = serverSocket.accept();
-            PrintWriter out =
-                new PrintWriter(clientSocket.getOutputStream(), true);                   
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);                   
+            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
         ) {
 	    	System.out.println("Accepted connection from " + clientSocket.getInetAddress());
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-            	System.out.println("Server received: " + inputLine);
-                out.println("From server: " + inputLine);
+            Object receivedObject;
+            while ((receivedObject = in.readObject()) != null) {
+            	System.out.println("Server received: " + receivedObject.getClass());
             }
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             System.out.println("Exception caught when trying to listen on port "
                 + tcpPort + " or listening for a connection");
             System.out.println(e.getMessage());
         }
 
+	    
+	    // END SAMPLE CODE
 	    
 	    
 
