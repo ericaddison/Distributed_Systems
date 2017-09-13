d60e1ff704c1ac081f6d3702bd6a48fb4b44a343
ericaddison <ericaddison@gmail.com>
2017-09-13 13:24:01 -0500

    Moved UDP read/write into new class UdpIO

---
+++ src/store/UdpIO.java
@@ -0,0 +1,45 @@
+package store;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UdpIO {
+	
+	public static ObjectAndPacket receiveObject(DatagramSocket sock, int bufLen) throws IOException{
+		DatagramPacket datapacket = new DatagramPacket(new byte[bufLen], bufLen);
+		sock.receive(datapacket);
+		
+		ObjectInputStream is = new ObjectInputStream(new ByteArrayInputStream(datapacket.getData()));
+		Object o = null;
+		try {
+			o = is.readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return new ObjectAndPacket(o, datapacket);
+	}
+	
+	
+	
+	public static void sendObject(Object o, InetAddress addr, int port, DatagramSocket sock) throws IOException{
+		
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutputStream os = new ObjectOutputStream(bos);
+		os.writeObject(o);
+		byte[] barray = bos.toByteArray();
+		
+		DatagramPacket returnpacket = new DatagramPacket(
+				barray,
+				barray.length,
+				addr,
+				port);
+		sock.send(returnpacket);
+		
+	}
+}
--- src/store/UdpServerTask.java
+++ src/store/UdpServerTask.java
@@ -1,13 +1,8 @@
 package store;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 
 public class UdpServerTask implements Runnable{
@@ -24,49 +19,15 @@ public class UdpServerTask implements Runnable{
 	}
 
 
-	public static ObjectAndPacket receiveObject(DatagramSocket sock) throws IOException{
-		int len = 1024;
-		DatagramPacket datapacket = new DatagramPacket(new byte[len], len);
-		sock.receive(datapacket);
-		
-		ObjectInputStream is = new ObjectInputStream(new ByteArrayInputStream(datapacket.getData()));
-		Object o = null;
-		try {
-			o = is.readObject();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return new ObjectAndPacket(o, datapacket);
-	}
-	
-	
-	
-	public static void sendObject(Object o, InetAddress addr, int port, DatagramSocket sock) throws IOException{
-		
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		ObjectOutputStream os = new ObjectOutputStream(bos);
-		os.writeObject(o);
-		byte[] barray = bos.toByteArray();
-		
-		DatagramPacket returnpacket = new DatagramPacket(
-				barray,
-				barray.length,
-				addr,
-				port);
-		sock.send(returnpacket);
-		
-	}
-
-
 	@Override
 	public void run() {
 		DatagramPacket datapacket;
 		try{
 			while (true) {
-				ObjectAndPacket oap = receiveObject(datasocket);
+				ObjectAndPacket oap = UdpIO.receiveObject(datasocket, 1024);
 				datapacket = oap.datagramPacket;
 				System.out.println("Received packet from " + datapacket.getAddress() + ": " + oap.object);
-				sendObject(oap.object, oap.datagramPacket.getAddress(), oap.datagramPacket.getPort(), datasocket);
+				UdpIO.sendObject(oap.object, oap.datagramPacket.getAddress(), oap.datagramPacket.getPort(), datasocket);
 			}
 		} catch (IOException e) {
 			System.err.println(e);
